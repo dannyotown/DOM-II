95cf13cd6730358d3921a84b254c4ffd31c241da
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-08-11 10:37:06 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,20 +19,24 @@ function navBar(){
     let arrayN = Array.from(nav);
     arrayN.forEach((item) => item.addEventListener('focus',function(){item.style.backgroundColor = '#C0C0C0';item.style.borderRadius = '5px'; item.style.padding = '2px';})) 
 }
-function blogScroll(){
+function keydownPress(){
     //not working
-    let nav = document.getElementsByClassName('nav-link');
-    let div = document.getElementsByClassName('container home');
-    div.scrollTop = div.scrollHeight;
-    nav[2].addEventListener('click', function(){window.scrollTo(0,div.scrollTop = div.scrollHeight);event.preventDefault()});
+    document.getElementsByTagName('body')[0].addEventListener('keydown',function(){alert('You Pressed A Key')});
 }
 function navDivBackground(){
     //not working
     let navDiv = document.getElementsByClassName('main-navigation');
-    document.getElementsByTagName('body')[0].addEventListener('scroll', function(){navDiv[0].style.backgroundColor="#FCBC4F"; event.stopPropagation();});
+    window.addEventListener('scroll',()=>{
+        navDiv[0].style.backgroundColor="#FCBC4F"; event.stopPropagation();
+    }) 
+    window.addEventListener('mouseover',()=>{
+        navDiv[0].style.backgroundColor="white"; event.stopPropagation();
+    }) 
+    
+    
 }
 navOnClick();
 btnBckGround();
 navBar();
-blogScroll();
+keydownPress();
 navDivBackground();
